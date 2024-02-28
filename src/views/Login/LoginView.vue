8a1863996bb9f9a97e826321f3b2ac1aa8e54166
trieutct <96654274+trieutct@users.noreply.github.com>
2024-02-28 11:13:20 +0700

    update reposive

--- src/views/Login/LoginView.vue
+++ src/views/Login/LoginView.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <v-card class="mx-auto pa-12 pb-8 my-6" variant="flat" max-width="500" rounded="lg">
+        <v-card class="mx-auto pa-12 pb-8" variant="flat" max-width="500" rounded="lg">
             <v-img class="mx-auto my-6" max-width="100"
                 src="https://tse4.mm.bing.net/th?id=OIP.fv5DeUz-OmV84NOdioiodgHaEc&pid=Api&P=0&h=220"></v-img>
-            <div class="text-h4 text-center">
+            <div style="font-family: Public Sans;font-weight: 600;" class="text-h4 text-center">
                 Đăng nhập
             </div>
             <v-form @submit.prevent="loginform.handleLogin" @keyup.enter="loginform.handleLogin">
@@ -19,7 +19,7 @@
                     @click:append-inner="visible = !visible"></v-text-field>
                 <div class="d-flex justify-space-between align-center">
                     <v-checkbox v-model="terms" color="secondary" label="Ghi nhớ Đăng nhập"></v-checkbox>
-                    <a class="text-caption text-decoration-none text-blue mb-6">
+                    <a style="font-size: 16px;" class="text-caption text-decoration-none text-blue mb-6">
                         Quên mật khẩu?</a>
                 </div>
                 <v-btn @click="loginform.handleLogin" @enter="loginform.handleLogin" block class="mb-8" color="#0F60FF"
@@ -80,4 +80,5 @@ const terms = ref('')
 // //     console.log('Logging in with:', email, password);
 // // });
 </script>
-<style></style>
+<style scoped>
+</style>
