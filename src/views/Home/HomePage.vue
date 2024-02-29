1ae4e360170abed532befdc9e4a216b988740923
trieutct <96654274+trieutct@users.noreply.github.com>
2024-02-29 12:18:37 +0700

    add UI new

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -14,7 +14,7 @@
             <v-col cols="1"></v-col>
         </v-row>
         </div>
-        <v-app-bar class="mt-10 px-4 v-app-bar" color="white" :elevation="0" rounded="0">
+        <v-toolbar class="px-4 v-app-bar" color="white" :elevation="0" rounded="0">
             <v-row>
                 <v-col cols="1"></v-col>
                 <v-col cols="10">
@@ -22,12 +22,6 @@
                         <h3 style="font-size: 32px;  font-family: Integral CF, sans-serif;font-weight: 700;color: #000000;">
                             SHOP.CO
                         </h3>
-                        <!-- <v-spacer></v-spacer>
-                        <v-btn>
-                            <v-badge content="5" color="red">
-                                <v-icon style="font-size: 20px;">mdi-bell-outline</v-icon>
-                            </v-badge>
-                        </v-btn> -->
                         <div style="margin-left: 40px;" class="d-flex align-center justify-center">
                             <p class="ma-2">Shop</p>
                             <p class="ma-2">On Sale</p>
@@ -54,6 +48,37 @@
                 </v-col>
                 <v-col cols="1"></v-col>
             </v-row>
-        </v-app-bar>
+        </v-toolbar>
+        <v-img style="ma" :src="image" contain>
+            <div class="fill-height absolute-fill" align="left" justify="center">
+               <v-row style="margin-top: 5%;">
+                <v-col cols="1"></v-col>
+                 <v-col cols="5">
+                    <p style="color: #000000;font-size:64px;font-weight:bold;line-height: 64px;">FIND CLOTHES THAT MATCHES YOUR STYLE</p>
+                    <p class="text-left" style="color: #000000;opacity: 0.6;margin-top: 32px">Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+                    <v-btn class="text-capitalize" style="color: white;background-color: #000000; width: 210px;height: 52px;font-size: 16px;margin-top: 32px;" rounded>Shop Now</v-btn>
+                    <v-row style="margin-top: 48px;height: 74px;">
+                        <div>
+                            <b style="font-size: 40px;">200 +</b>
+                            <p style="opacity: 0.6;font-size: 16px;">International Brands</p>
+                        </div>
+                        <v-divider vertical class="mx-5 border-opacity-100"></v-divider>
+                        <div>
+                            <b style="font-size: 40px;">2,000 +</b>
+                            <p style="opacity: 0.6;font-size: 16px;">High-Quality Products</p>
+                        </div>
+                        <v-divider vertical class="mx-5 border-opacity-100"></v-divider>
+                        <div>
+                            <b style="font-size: 40px;">30,000 +</b>
+                            <p style="opacity: 0.6;font-size: 16px;">Happy Customers</p>
+                        </div>
+                    </v-row>
+                 </v-col>
+               </v-row>
+            </div>
+        </v-img>
     </v-app>
 </template>
+<script setup>
+import image from "@/assets/slideer.png"
+</script>
